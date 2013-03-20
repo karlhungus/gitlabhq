10a9d6424225b39c6decea3e61a998d30cdb2c78
Izaak Alpert <ihalpert@blackberry.com>
2013-03-20 13:23:08 -0400

    Make BB theme default
    -DB was defaulting to theme #1, rather then change all db defaults i just changed the default theme to be the bb theme, and pushed all the other ones down one (i.e.basic went from 1 to 2).
    -Didn't git rm the icons-04.png when i commited GITLAB-76, so cleaning this up
    
    GITLAB-5
    
    Change-Id: Ibef32178503f99fa30720c1c62daf7e6c135a30a

--- lib/gitlab/theme.rb
+++ lib/gitlab/theme.rb
@@ -2,16 +2,14 @@ module Gitlab
   class Theme
     def self.css_class_by_id(id)
       themes = {
-        1 => "ui_basic",
-        2 => "ui_mars",
-        3 => "ui_modern",
-        4 => "ui_gray",
-        5 => "ui_color",
-	6 => "ui_bb"
+        1 => "ui_bb",
+        2 => "ui_basic",
+        3 => "ui_mars",
+        4 => "ui_modern",
+        5 => "ui_gray",
+        6 => "ui_color"
       }
-
-      id ||= 6 
-
+      id ||= 1
       return themes[id]
     end
   end
